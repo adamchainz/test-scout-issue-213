05d61e0a008d1052da20ef362325f172938b5723
Adam Johnson <me@adamj.eu>
2019-07-22 09:37:52 +0100

    Set up scout debug logging

--- manage.py
+++ manage.py
@@ -9,15 +9,51 @@ from django.urls import path
 from django.utils.crypto import get_random_string
 
 settings.configure(
-    DEBUG=(os.environ.get("DEBUG", "") == "1"),
+    # Django core
     ALLOWED_HOSTS=["*"],  # Disable host header validation
-    ROOT_URLCONF=__name__,  # Make this module the urlconf
-    SECRET_KEY=get_random_string(50),  # We aren't using any security features but Django requires this setting
+    DEBUG=(os.environ.get("DEBUG", "") == "1"),
     INSTALLED_APPS=[
         'django.contrib.staticfiles',
         'scout_apm.django',
     ],
+    LOGGING={
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'stdout': {
+                'format': '%(asctime)s %(levelname)s %(message)s',
+                'datefmt': '%Y-%m-%dT%H:%M:%S%z',
+            },
+        },
+        'handlers': {
+            'stdout': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'stdout',
+            },
+            'scout_apm': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': 'scout_apm_debug.log',
+            },
+        },
+        'root': {
+            'handlers': ['stdout'],
+            'level': os.environ.get('LOG_LEVEL', 'DEBUG'),
+        },
+        'loggers': {
+            # as per https://docs.scoutapm.com/#django-logging
+            'scout_apm': {
+                'handlers': ['scout_apm'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    },
+    ROOT_URLCONF=__name__,  # Make this module the urlconf
+    SECRET_KEY=get_random_string(50),  # We aren't using any security features but Django requires this setting
+    # django.contrib.staticfiles
     STATIC_URL='/static/',
+    # scout_apm
     SCOUT_MONITOR=True,
     SCOUT_KEY=os.environ['SCOUT_KEY'],
     SCOUT_NAME="Test App",
